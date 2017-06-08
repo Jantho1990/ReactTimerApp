cec4395451243a7c19ce76251d2c8a15182176f6
Joshua Anthony <jantho1990@gmail.com>
2017-06-07 20:00:27 -0500

    added startTimer method to Countdown component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,11 +5,41 @@ const React = require('react'),
 var Countdown = React.createClass({
   getInitialState: function(){
     return {
-      count: 0
+      count: 0,
+      coundownStatus: 'stopped'
     }
   },
+  componentDidUpdate: function(prevProps, prevState){
+    if(this.state.countdownStatus !== prevState.countdownStatus){
+      switch(this.state.countdownStatus){
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+
+          break;
+      }
+    }
+  },
+  startTimer: function(){
+    this.timer = setInterval(() => {
+      let newCount = this.state.count - 1;
+      console.log(newCount);
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+      if(newCount <= 0){this.stopTimer();}
+
+    }, 1000);
+  },
+  stopTimer: function(){
+    clearInterval(this.timer);
+  },
   handleSetCountdown: function(seconds){
-    this.setState({count: seconds});
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
   },
   render: function(){
     let {count} = this.state;
