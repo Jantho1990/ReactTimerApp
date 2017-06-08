d8770014146477f68e252ac4deeb7edbd0559e36
Joshua Anthony <jantho1990@gmail.com>
2017-06-07 19:35:03 -0500

    added tests for countdownform component

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -10,31 +10,30 @@ describe('Clock', () => {
   it('should exist', () => {
     expect(Clock).toExist();
   });
-});
+  describe('render', () => {
+    it('should render clock to output', () => {
+      let clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+      let $el = $(ReactDOM.findDOMNode(clock));
+      let actualText = $el.find('.clock-text').text();
 
-describe('render', () => {
-  it('should render clock to output', () => {
-    let clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
-    let $el = $(ReactDOM.findDOMNode(clock));
-    let actualText = $el.find('.clock-text').text();
-
-    expect(actualText).toBe('01:02');
+      expect(actualText).toBe('01:02');
+    });
   });
-});
 
-describe('formatSeconds', () => {
-  it('should format seconds', () => {
-    let clock = TestUtils.renderIntoDocument(<Clock/>);
-    let seconds = 615, expected = '10:15';
-    let actual = clock.formatSeconds(seconds);
+  describe('formatSeconds', () => {
+    it('should format seconds', () => {
+      let clock = TestUtils.renderIntoDocument(<Clock/>);
+      let seconds = 615, expected = '10:15';
+      let actual = clock.formatSeconds(seconds);
 
-    expect(actual).toBe(expected);
-  });
-  it('should format seconds when min/sec less than 10', () => {
-    let clock = TestUtils.renderIntoDocument(<Clock/>);
-    let seconds = 61, expected = '01:01';
-    let actual = clock.formatSeconds(seconds);
+      expect(actual).toBe(expected);
+    });
+    it('should format seconds when min/sec less than 10', () => {
+      let clock = TestUtils.renderIntoDocument(<Clock/>);
+      let seconds = 61, expected = '01:01';
+      let actual = clock.formatSeconds(seconds);
 
-    expect(actual).toBe(expected);
+      expect(actual).toBe(expected);
+    });
   });
 });
