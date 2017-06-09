564628e32914ee41faf66f8b6f1b5d7fb93aaf8d
Joshua Anthony <jantho1990@gmail.com>
2017-06-08 21:27:08 -0500

    added test for countdown handleSetCountdown

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,38 @@
+const React     = require('react'),
+      ReactDOM  = require('react-dom'),
+      expect    = require('expect'),
+      $         = require('jquery'),
+      TestUtils = require('react-addons-test-utils');
+
+const Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCoundown', () => {
+    it('should set state to started and countdown', (done) => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      }, 1001);
+    });
+    it('should never be less than zero', () => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 3001);
+    });
+  });
+
+});
